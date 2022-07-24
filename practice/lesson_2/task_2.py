0bebed62091168401b9b0acefa587c3dcde8ed5b
Galprim <galprimulus@gmail.com>
2022-07-25 02:11:55 +0300

    add lesson 2 task 2

---
+++ practice/lesson_2/task_2.py
@@ -0,0 +1,23 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+num = int(input('Введите целое число: '))
+
+def like_factorial (n):
+    a = [1]
+    if n == 0:
+        return a
+
+    elif n < 0:
+        mess = 'Вы ввели отрицательное значение'
+        return mess
+
+    else:
+        res = 1
+        for i in range(2, n + 1):
+            res *= i
+            a.append(res)
+        return a
+
+print(like_factorial(num))
