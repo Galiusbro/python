ccb39721bf3823e81adbedb0290084f36dff542d
Galprim <galprimulus@gmail.com>
2022-07-27 04:02:44 +0300

    add lesson 3 task 1

--- lection/lesson2.py
+++ lection/lesson2.py
@@ -1,10 +1,17 @@
-my_file = open("BabyFile.txt", "w+")
-my_file.write("Привет, файл!")
-my_file.close()
+# my_file = open("BabyFile.txt", "w+")
+# my_file.write("Привет, файл!")
+# my_file.close()
 
-exit()
-path = 'file.txt'
-data = open(path, 'r')
-for line in data:
-    print(line)
-data.close()
+# exit()
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+
+import time
+
+a = str(time.time())
+a = a[-1] + a[-2]
+print(a)
