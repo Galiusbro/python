2b8d9b84b6aea8af44065a501ca7236273fff6aa
Galprim <galprimulus@gmail.com>
2022-08-18 21:43:28 +0300

    add lesson 5 task 3

--- practice/lesson_5/task_1.py
+++ practice/lesson_5/task_1.py
@@ -9,5 +9,6 @@ for word in start_str_split:
     if root not in word.lower():
         new_set_of_word.append(word)
 
+
 new_str = " ".join(map(str, new_set_of_word))
 print(new_str)
--- practice/lesson_5/task_2.py
+++ practice/lesson_5/task_2.py
@@ -10,7 +10,8 @@
 from random import randint
 
 
-candies = 221
+candies = 120
+take_limit = 28
 player_1 = 1
 player_2 = 2
 
@@ -20,20 +21,31 @@ print(f'Выпало = {turn}\n')
 print(f'Осталось {candies} конфет\n')
 
  
-while candies >= 0:
-    if turn == 1:
-        print('Бери конфеты, ИГРОК!')
-        take_palyer_1 = int(input('Сколько конфет берет игрок?: '))
-        while turn == 1:
-            if take_palyer_1 > 0 and take_palyer_1 < 29:
-                candies = candies - take_palyer_1
-                print(f'Осталось {candies} конфет\n')
-                turn = 0
-            else: take_palyer_1 = int(input('Вы можете взять от 1 до 28 конфет! :'))
+while candies > 0:
+  if candies < take_limit: take_limit = candies
+  if turn == 1:
+      print('Бери конфеты, ИГРОК!')
+      take_palyer_1 = int(input('Сколько конфет берет игрок?: '))
+      while turn == 1:
+          if take_palyer_1 > 0 and take_palyer_1 < take_limit +1:
+              candies = candies - take_palyer_1
+              print(f'Осталось {candies} конфет!\n')
+              turn = 0
+          else: take_palyer_1 = int(input(f'Вы можете взять от 1 до {take_limit} конфет! :'))
+            
     
+  else:
+    if candies > 28:
+      take_player_2 = randint(1, take_limit)
     else:
-        take_player_2 = randint(1, 28)
-        print(f'Комп взял {take_player_2} конфет')
-        candies = candies - take_player_2
-        print(f'Осталось {candies} конфет\n')
-        turn = 1
+      take_player_2 = candies
+    print(f'Комп взял {take_player_2} конфет')
+    candies = candies - take_player_2
+    print(f'Осталось {candies} конфет!\n')
+    turn = 1
+
+if turn == 0:
+  print('YOU WIN!')
+else:
+  print('LOSER!')
+  
